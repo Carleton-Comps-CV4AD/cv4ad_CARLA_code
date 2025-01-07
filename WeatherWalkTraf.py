9716f0387d40c7f2d2dccbd26e466f7939b2cd60
nli00 <email.nathaniel.li@gmail.com>
2025-01-07 12:33:28 -0600

    testing weather configuration

--- WeatherWalkTraf.py
+++ WeatherWalkTraf.py
@@ -11,12 +11,12 @@
 import glob
 import os
 import sys
-import carla # type: ignore
+import carla # type: ignore <-- what is this
 import random
 import time
 import math
-import argparse
 import math
+import yaml
 
 # Global configuration
 SECONDS_PER_TICK = 5.0 # seconds per tick
@@ -41,62 +41,35 @@ class Sun(object):
         self.altitude = altitude
         self._t = 0.0
 
-    def tick(self, delta_seconds):
-        self._t += 0.008 * delta_seconds
-        self._t %= 2.0 * math.pi
-        self.azimuth += 0.25 * delta_seconds
-        self.azimuth %= 360.0
-        self.altitude = (70 * math.sin(self._t)) - 20
+    def set_azimuth(self, azimuth):
+        self.azimuth = azimuth
+    
+    def set_altitude(self, altitude):
+        self.altitude = altitude
 
     def __str__(self):
         return 'Sun(alt: %.2f, azm: %.2f)' % (self.altitude, self.azimuth)
 
 
-class Storm(object):
-    def __init__(self, precipitation):
-        self._t = precipitation if precipitation > 0.0 else -50.0
-        self._increasing = True
-        self.clouds = 0.0
-        self.rain = 0.0
-        self.wetness = 0.0
-        self.puddles = 0.0
-        self.wind = 0.0
-        self.fog = 0.0
-
-    def tick(self, delta_seconds):
-        delta = (1.3 if self._increasing else -1.3) * delta_seconds
-        self._t = clamp(delta + self._t, -250.0, 100.0)
-        self.clouds = clamp(self._t + 40.0, 0.0, 90.0)
-        self.rain = clamp(self._t, 0.0, 80.0)
-        delay = -10.0 if self._increasing else 90.0
-        self.puddles = clamp(self._t + delay, 0.0, 85.0)
-        self.wetness = clamp(self._t * 5, 0.0, 100.0)
-        self.wind = 5.0 if self.clouds <= 20 else 90 if self.clouds >= 70 else 40
-        self.fog = clamp(self._t - 10, 0.0, 30.0)
-        if self._t == -250.0:
-            self._increasing = True
-        if self._t == 100.0:
-            self._increasing = False
-
-    def __str__(self):
-        return 'Storm(clouds=%d%%, rain=%d%%, wind=%d%%)' % (self.clouds, self.rain, self.wind)
-
-
 class Weather(object):
-    def __init__(self, weather):
+    def __init__(self, weather, configs):
         self.weather = weather
         self._sun = Sun(weather.sun_azimuth_angle, weather.sun_altitude_angle)
-        self._storm = Storm(weather.precipitation)
+        with open(configs, 'r') as file:
+            self.states = iter(yaml.safe_load(file)['states'])
 
-    def tick(self, delta_seconds):
-        self._sun.tick(delta_seconds)
-        self._storm.tick(delta_seconds)
-        self.weather.cloudiness = self._storm.clouds
-        self.weather.precipitation = self._storm.rain
-        self.weather.precipitation_deposits = self._storm.puddles
-        self.weather.wind_intensity = self._storm.wind
-        self.weather.fog_density = self._storm.fog
-        self.weather.wetness = self._storm.wetness
+    def next(self):
+        state = self.states.next()
+        # self._sun.set_azimuth(state.azimuth) see the comment on alititude in the yaml
+        self._sun.set_alititude(state.altitude)
+
+        self.weather.cloudiness = self.clouds
+        self.weather.precipitation = self.rain
+        self.weather.precipitation_deposits = self.puddles
+        self.weather.wind_intensity = self.wind
+        self.weather.fog_density = self.fog
+        self.weather.wetness = self.wetness
+        
         self.weather.sun_azimuth_angle = self._sun.azimuth
         self.weather.sun_altitude_angle = self._sun.altitude
 
@@ -135,6 +108,84 @@ def save_image(image, counter, name, file_type, cc = None):
         image.save_to_disk(image_path)
     counter.pp()
 
+# ! Temporary function to fill the scene with vehicles and pedestrians. We should paratmetrize and organize this so that we can configure the scene easily
+def initialize_agents(world, client, actor_list, traffic_manager):
+    # Select some models from the blueprint library
+    models = ['dodge', 'audi', 'model3', 'mini', 'mustang', 'lincoln', 'prius', 'nissan', 'crown', 'impala']
+    blueprints = []
+    for v in world.get_blueprint_library().filter('*vehicle*'):
+        if any(model in v.id for model in models):
+            blueprints.append(v)
+
+    # Set a max number of vehicles and prepare a list for those we spawn
+    max_vehicles = 20
+    max_vehicles = min([max_vehicles, len(spawn_points)])
+    vehicles = []
+    
+    for i, spawn_point in enumerate(random.sample(spawn_points, max_vehicles)):
+        temp = world.try_spawn_actor(random.choice(blueprints), spawn_point)
+        if temp is not None:
+            vehicles.append(temp)
+
+    for v in vehicles:
+        v.set_autopilot(True) 
+        traffic_manager.random_left_lanechange_percentage(v, 0)
+        traffic_manager.random_right_lanechange_percentage(v, 0)
+        traffic_manager.auto_lane_change(v, False)  
+    
+    # Spawn walkers
+    blueprint_library = world.get_blueprint_library()
+    walker_bp = blueprint_library.filter('walker.pedestrian.*')
+    walker_controller_bp = blueprint_library.find('controller.ai.walker')
+
+
+    walkers = []
+    controllers = []
+
+
+    spawn_points = []
+    for i in range(100):
+        spawn_point = carla.Transform()
+        spawn_point.location = world.get_random_location_from_navigation()
+        if spawn_point.location is not None:
+            spawn_points.append(spawn_point)
+
+
+    sbatch = []
+    for spawn_point in spawn_points:
+        walker_bp_choice = random.choice(walker_bp)
+        sbatch.append(carla.command.SpawnActor(walker_bp_choice, spawn_point))
+
+    results = client.apply_batch_sync(sbatch, True)
+
+    for result in results:
+        if not result.error:
+            walkers.append(result.actor_id)
+
+    batch = []
+    for walker_id in walkers:
+        batch.append(carla.command.SpawnActor(walker_controller_bp, carla.Transform(), walker_id))
+
+    results = client.apply_batch_sync(batch, True)
+
+    for result in results:
+        if not result.error:
+            controllers.append(result.actor_id)
+
+    walker_actors = [world.get_actor(w) for w in walkers]
+    controller_actors = [world.get_actor(c) for c in controllers]
+
+    for controller in controller_actors:
+        controller.start()
+        controller.go_to_location(world.get_random_location_from_navigation())
+        controller.set_max_speed(random.uniform(1.0, 3.0))  # Random speeds
+
+    print(f"Spawned {len(walkers)} walkers.")
+
+    actor_list.extend(walker_actors)
+    actor_list.extend(controller_actors)
+
+    return vehicles
 
 def main():
     actor_list = []
@@ -164,8 +215,6 @@ def main():
         # The world contains the list blueprints that we can use for adding new
         # actors into the simulation.
         blueprint_library = world.get_blueprint_library()
-
-        weather = Weather(world.get_weather())
 
         # Now let's filter all the blueprints of type 'vehicle' and choose one
         # at random.
@@ -241,81 +290,6 @@ def main():
 
         # traffic_manager.set_path(vehicle, route_1)
 
-        # Select some models from the blueprint library
-        models = ['dodge', 'audi', 'model3', 'mini', 'mustang', 'lincoln', 'prius', 'nissan', 'crown', 'impala']
-        blueprints = []
-        for v in world.get_blueprint_library().filter('*vehicle*'):
-            if any(model in v.id for model in models):
-                blueprints.append(v)
-
-        # Set a max number of vehicles and prepare a list for those we spawn
-        max_vehicles = 20
-        max_vehicles = min([max_vehicles, len(spawn_points)])
-        vehicles = []
-        
-        for i, spawn_point in enumerate(random.sample(spawn_points, max_vehicles)):
-            temp = world.try_spawn_actor(random.choice(blueprints), spawn_point)
-            if temp is not None:
-                vehicles.append(temp)
-
-        for v in vehicles:
-            v.set_autopilot(True) 
-            traffic_manager.random_left_lanechange_percentage(v, 0)
-            traffic_manager.random_right_lanechange_percentage(v, 0)
-            traffic_manager.auto_lane_change(v, False)  
-        
-        # Spawn walkers
-        blueprint_library = world.get_blueprint_library()
-        walker_bp = blueprint_library.filter('walker.pedestrian.*')
-        walker_controller_bp = blueprint_library.find('controller.ai.walker')
-
-
-        walkers = []
-        controllers = []
-
-
-        spawn_points = []
-        for i in range(100):
-            spawn_point = carla.Transform()
-            spawn_point.location = world.get_random_location_from_navigation()
-            if spawn_point.location is not None:
-                spawn_points.append(spawn_point)
-
-
-        sbatch = []
-        for spawn_point in spawn_points:
-            walker_bp_choice = random.choice(walker_bp)
-            sbatch.append(carla.command.SpawnActor(walker_bp_choice, spawn_point))
-
-        results = client.apply_batch_sync(sbatch, True)
-
-        for result in results:
-            if not result.error:
-                walkers.append(result.actor_id)
-
-        batch = []
-        for walker_id in walkers:
-            batch.append(carla.command.SpawnActor(walker_controller_bp, carla.Transform(), walker_id))
-
-        results = client.apply_batch_sync(batch, True)
-
-        for result in results:
-            if not result.error:
-                controllers.append(result.actor_id)
-
-        walker_actors = [world.get_actor(w) for w in walkers]
-        controller_actors = [world.get_actor(c) for c in controllers]
-
-        for controller in controller_actors:
-            controller.start()
-            controller.go_to_location(world.get_random_location_from_navigation())
-            controller.set_max_speed(random.uniform(1.0, 3.0))  # Random speeds
-
-        print(f"Spawned {len(walkers)} walkers.")
-
-        actor_list.extend(walker_actors)
-        actor_list.extend(controller_actors)
-
         # # But the city now is probably quite empty, let's add a few more
         # # vehicles.
         # transform.location += carla.Location(x=40, y=-3.2)
@@ -333,23 +307,29 @@ def main():
         #         npc.set_autopilot(True)
         #         print('created %s' % npc.type_id)
 
-        elapsed_time = 0
+        vehicles = initialize_agents(world, client, actor_list, traffic_manager)
 
         t_end = time.time() + 2520
-        while time.time() < t_end:
+
+        weather = Weather(world.get_weather())
+        world.set_weather(weather.weather)
+        # while time.time() < t_end:
+        while True:
             world.tick()
-            timestamp = world.wait_for_tick(seconds=30.0).timestamp
-            elapsed_time += timestamp.delta_seconds
-            weather.tick(1 * elapsed_time)
-            world.set_weather(weather.weather)
-            sys.stdout.write('\r' + str(weather) + 12 * ' ')
-            sys.stdout.flush()
+            if rgb_cam_counter.value % 2 > 2:
+                try:
+                    weather.next()
+                except StopIteration:
+                    break
+                world.set_weather(weather.weather)
 
     finally:
+        for camera in ego.cameras: # For some reason, this seems to be necessary evewn though the cameras should be in the actor list, which is destroyed below. Investigate.
+            camera.destroy()
 
         print('destroying actors')
         client.apply_batch([carla.command.DestroyActor(x) for x in actor_list])
-        client.apply_batch([carla.command.DestroyActor(x) for x in vehicles])
+        client.apply_batch([carla.command.DestroyActor(x) for x in vehicles]) # Why is this separate from the actor list above?
         print('done.')
 
 
